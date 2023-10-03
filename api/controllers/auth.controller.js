9eb7aeaecacf14bc282449c25b188620a6b9b46c
Franco Alvarez <francoalvarezstevenot@hotmail.com>
2023-10-03 15:56:05 -0300

    sign in finished

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,11 +20,13 @@ export const signIn = async(req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if(!validUser) return next(errorHandler(404, 'User not found'));
+        if (!validUser) return next(errorHandler(404, 'User not found'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
         //este id es el que crea mongo
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
+        //acá le devuelvo toda la info al usuario menos su contraseña
+        const { password: pass, ...rest } = validUser._doc;
         res
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
