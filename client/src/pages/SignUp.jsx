1af8c92cc8f6f06d3e46b71ca331c2ec48d67712
Franco Alvarez <francoalvarezstevenot@hotmail.com>
2023-10-03 14:54:37 -0300

    sign up functional

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
 
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -33,7 +34,8 @@ export default function SignUp() {
         return;
       }
       setLoading(false);
-      
+      setError(null);
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(error.message);
@@ -43,7 +45,7 @@ export default function SignUp() {
   console.log(formData);
 
   return (
-    <div className='p-3 max-4-lg mx-auto'>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}></input>
