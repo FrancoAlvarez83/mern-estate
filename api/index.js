4f2828112692937c487306a0d4fecf61c7a51373
Franco Alvarez <francoalvarezstevenot@hotmail.com>
2023-10-03 15:25:41 -0300

    sign in first step

--- api/index.js
+++ api/index.js
@@ -15,9 +15,9 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server running on port 3000');
+// });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
